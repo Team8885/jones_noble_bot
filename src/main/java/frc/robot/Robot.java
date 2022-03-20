8febb593e4f70acb8c72569a483c5ec1d872064c
Jim W Jones <jim@irielife.org>
2022-03-20 15:04:29 -0700

    In file Robot.java, remove unused code, remove 2nd joystick, and rationalize object names.
    In class Robot, change object name of DifferentialDrive from m_myRobot to m_myDrive.
    Also in class Robot, remove 2nd joystick and change name of single instance to m_joyStick.
    
    In class RobotInit, remove obsolete CANSparkMax comments.
    
    In method teleopPeriodic, map the left motor to joystick Y-axis, right motor to X-axis.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,40 +13,22 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
-  //private static final int leftDeviceID = 1; 
-  //private static final int rightDeviceID = 2;
+  private DifferentialDrive m_myDrive;
+  private Joystick m_joyStick;
   private Spark m_leftMotor;
   private Spark m_rightMotor;
 
   @Override
   public void robotInit() {
-  /**
-   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
-   * 
-   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
-   * first parameter
-   * 
-   * The motor type is passed as the second parameter. Motor type can either be:
-   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
-   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
-   * 
-   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
-   * these parameters to match your setup
-   */
-    m_leftMotor = new Spark(0);
+  
+    m_leftMotor  = new Spark(0);
     m_rightMotor = new Spark(2);
-
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
-
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    m_myDrive    = new DifferentialDrive(m_leftMotor, m_rightMotor);
+    m_joyStick   = new Joystick(0);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    m_myDrive.tankDrive(m_joyStick.getY(), m_joyStick.getX());
   }
 }
