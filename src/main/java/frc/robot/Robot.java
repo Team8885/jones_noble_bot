79fe039c7c7ebedfe29c118ccedc1a9e42f0fb9c
Jim W Jones <jim@irielife.org>
2022-03-22 19:17:05 -0700

    in file Robot.java, replace Joystick with XboxController, and invert the left motor.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,12 +9,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.simulation.XboxControllerSim;
 
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myDrive;
-  private Joystick m_joyStick;
+  private XboxController m_joyStick;
   private Spark m_leftMotor;
   private Spark m_rightMotor;
 
@@ -23,12 +25,13 @@ public class Robot extends TimedRobot {
   
     m_leftMotor  = new Spark(0);
     m_rightMotor = new Spark(2);
+    m_leftMotor.setInverted(true);
     m_myDrive    = new DifferentialDrive(m_leftMotor, m_rightMotor);
-    m_joyStick   = new Joystick(0);
+    m_joyStick   = new XboxController(0);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myDrive.tankDrive(m_joyStick.getY(), m_joyStick.getX());
+    m_myDrive.tankDrive(m_joyStick.getLeftY(), m_joyStick.getRightY());
   }
 }
